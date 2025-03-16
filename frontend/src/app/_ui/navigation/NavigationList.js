2dda8553cd87806e7ca34522479a5d04fd9d1ba7
Kseniya Volodar <cucumber12@bk.ru>
2025-03-16 10:57:24 +0500

    header-icons added/done

--- frontend/src/app/_ui/navigation/NavigationList.js
+++ frontend/src/app/_ui/navigation/NavigationList.js
@@ -1,6 +1,9 @@
 'use client';
 import Link from "next/link";
 import {usePathname} from "next/navigation";
+import {UserCircleIcon} from "@heroicons/react/24/outline";
+import {ArrowRightEndOnRectangleIcon} from "@heroicons/react/24/outline";
+import {UserPlusIcon} from "@heroicons/react/24/outline";
 
 export default function NavigationList({isLoggedIn}){
     const pathName = usePathname();
@@ -11,14 +14,16 @@ export default function NavigationList({isLoggedIn}){
                     {pathName !== '/login' && <li className='h-5 sm:h-fit'>
                         <Link href="/login" className="text-sm sm:text-base md:text-xl
           text-primary-10 hover:text-accent-10 transition-colors ">
-                            Войти
+                            <ArrowRightEndOnRectangleIcon className='block sm:hidden w-11'/>
+                            <span className='hidden sm:block'>Войти</span>
                         </Link>
                     </li>
                     }
                     {pathName !== '/register' && <li className='h-5 sm:h-fit'>
                         <Link href="/register" className="text-sm sm:text-base md:text-xl
           text-primary-10 hover:text-accent-10 transition-colors">
-                            Регистрация
+                            <UserPlusIcon className='block sm:hidden w-11'/>
+                            <span className='hidden sm:block'>Регистрация</span>
                         </Link>
                     </li>
                     }
@@ -27,7 +32,9 @@ export default function NavigationList({isLoggedIn}){
             {(isLoggedIn && pathName !== '/account') && <li className='h-5 sm:h-fit'>
                 <Link href="/account" className="text-sm sm:text-base md:text-xl
                                             text-primary-10 hover:text-accent-10 transition-colors ">
-                    Аккаунт
+
+                 <UserCircleIcon className='block sm:hidden w-11'/>
+                 <span className='hidden sm:block'>Аккаунт</span>
                 </Link>
             </li>}
         </>
--- frontend/src/app/_ui/navigation/NavigationMain.js
+++ frontend/src/app/_ui/navigation/NavigationMain.js
@@ -1,5 +1,6 @@
 import {auth, signOut} from "@/auth";
 import NavigationList from "@/app/_ui/navigation/NavigationList";
+import {ArrowRightStartOnRectangleIcon} from "@heroicons/react/24/outline";
 
 export default  async function NavigationMain() {
   const session = await auth();
@@ -9,7 +10,7 @@ export default  async function NavigationMain() {
       <nav className="z-10 text-xl">
         <ul className="flex
                       pr-2 sm:pr-0
-                      flex-col sm:flex-row sm:items-center
+                      flex-row sm:items-center
                       gap-1 sm:gap-8 md:gap-10">
             <NavigationList isLoggedIn={isLoggedIn}/>
             {isLoggedIn && <li className='h-5 sm:h-fit'>
@@ -19,7 +20,9 @@ export default  async function NavigationMain() {
                 }}>
                     <button type="submit" className="text-sm sm:text-base md:text-xl
                                               text-primary-10 hover:text-accent-10 transition-colors ">
-                        Выйти
+                        <ArrowRightStartOnRectangleIcon
+                            className='block sm:hidden pt-1 flex items-center justify-center w-10'/>
+                        <span className='hidden sm:block'>Выйти</span>
                     </button>
                 </form>
             </li>}
